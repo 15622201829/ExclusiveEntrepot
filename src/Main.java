42faa5d7425f9049d93a4ea1ed05a4f64af7400c
wulinquan <wulinquan@do1.com.cn>
2018-06-13 12:04:01 +0800

    再次测试分支操作！

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("新增分支操作！");
+        System.out.println("测试分支操作！");
     }
 }
